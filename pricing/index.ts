ed0c50b083c97d21f47e4a0755b28cdb0717b2f2
joshstevens19 <joshstevens19@hotmail.co.uk>
2024-05-15 10:00:00 +0000

    feat: export evaluation modules

---
+++ pricing/index.ts
@@ -0,0 +1,5 @@
+export { BasePricer, saveBettor, loadBettor } from "./basePricer.js";
+export { ModelPricer } from "./modelPricer.js";
+export { MarketEdgePricer } from "./marketEdgePricer.js";
+export { backtest } from "../portfolio/backtestRunner.js";
+export type { BacktestRow } from "../portfolio/backtestRunner.js";
